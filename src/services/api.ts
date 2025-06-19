c21ccadd89682ad385379123bcd5b880f4069157
Ahmed <aa87006722@gmail.com>
2025-06-19 15:53:06 +0300

    Fix Some Issues

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,20 +1,19 @@
 
 // For Search About Cities, It Get Cities Add Information About It
 // It Has namePrefix Which Will Be Very Useful When Search , and more .. 
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const GEO_API_URL = (process.env.REACT_APP_GEO_API_BASE_URL as string);
 
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': 'ebd1a78d91msh7cb9a642764959fp121ddajsnfe0b6c34bdd2',
-		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+		'x-rapidapi-key': (process.env.REACT_APP_GEO_API_KEY as string),
+		'x-rapidapi-host': (process.env.REACT_APP_GEO_API_HOST as string),
 	}
 };
 
 // API To Get Current User IP
-export const IPINFO_API_URL = "https://ipinfo.io/json";
+export const IPINFO_API_URL = (process.env.REACT_APP_IPINFO_API_BASE_URL as string);
 
 // Weather API ( Get Weather Data Based On Location )
-const WEATHER_API_KEY = "6a6672e6e55047b5a3690358252701";
-export const WEATHER_API_FORECAST = `http://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&days=3&aqi=yes&alerts=yes`;
-export const WEATHER_API_HISTORY = `http://api.weatherapi.com/v1/history.json?key=${WEATHER_API_KEY}`;
+export const WEATHER_API_FORECAST = (process.env.REACT_APP_WEATHER_API_FORECAST_BASE_URL as string);
+export const WEATHER_API_HISTORY = (process.env.REACT_APP_WEATHER_API_HISTORY_BASE_URL as string);
