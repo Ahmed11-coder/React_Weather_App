227ccdc57f03acb5a0e7abb5ee6c817b393c677f
Ahmed <aa87006722@gmail.com>
2025-06-22 10:14:02 +0300

    ( Config ) Implement custom Webpack config with babel-loader , DefinePlugin

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require('webpack');
+require('dotenv').config({ path: './.env' }); 
+
+module.exports = {
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                "@babel/preset-env",
+                                ["@babel/preset-react", {"runtime": "automatic"}],
+                                "@babel/preset-typescript",
+                            ],
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpe?g)$/i,
+                type: "asset/resource",
+            }
+        ],
+    },
+    devtool: "source-map",
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+        new webpack.DefinePlugin({
+            'process.env': JSON.stringify(process.env),
+        })
+    ],
+
+    resolve: {
+        alias: {
+            '@utils': path.resolve(__dirname, './src/utils'),
+            '@store': path.resolve(__dirname, './src/store'),
+            '@services': path.resolve(__dirname, './src/services'),
+            '@data': path.resolve(__dirname, './src/data'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@hooks': path.resolve(__dirname, './src/hooks'),
+        },
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+            serveIndex: false,
+        },
+        hot: true,
+        historyApiFallback: true,
+        port: 3000,
+        open: true,
+    },
+    mode: "development"
+}
