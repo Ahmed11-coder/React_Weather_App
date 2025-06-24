030cdfc48b99d1175d61260accceb4a98324d002
Ahmed <aa87006722@gmail.com>
2025-06-24 15:29:11 +0300

    ( Chore ) Remove Unused Items

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         publicPath: '/',
     },
     devServer: {
