ed716fa1326dcd562d5dd5255b0b71e555fccc7c
Ahmed <aa87006722@gmail.com>
2025-06-26 18:14:30 +0300

    ( Feat ) Add Pexels Api For Random Background

--- src/services/api.ts
+++ src/services/api.ts
@@ -17,3 +17,14 @@ export const IPINFO_API_URL = (process.env.REACT_APP_IPINFO_API_BASE_URL as stri
 // Weather API ( Get Weather Data Based On Location )
 export const WEATHER_API_FORECAST = (process.env.REACT_APP_WEATHER_API_FORECAST_BASE_URL as string);
 export const WEATHER_API_HISTORY = (process.env.REACT_APP_WEATHER_API_HISTORY_BASE_URL as string);
+
+// Pexels API
+export const PEXELS_API_URL_DESKTOP = (process.env.REACT_APP_PEXELS_API_BASE_URL_DESKTOP as string);
+export const PEXELS_API_URL_MOBILE = (process.env.REACT_APP_PEXELS_API_BASE_URL_MOBILE as string);
+
+export const pexelsApiOptions = {
+	method: 'GET',
+	headers: {
+		Authorization: "M2UdQKOPRqhNonk0Gi79SG1voSTnUdO9tnHu09jl0ozm731QpV72ktD4"
+	}
+}
