b3241108ffdbea58a573acf962e6125901da4e8a
Ahmed <aa87006722@gmail.com>
2025-06-30 00:26:24 +0300

    ( Fix ) Using Unsplash API Rather Than Pexels API For More Accurate Images

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,7 +6,7 @@ export const GEO_API_URL = (process.env.REACT_APP_GEO_API_BASE_URL as string);
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': (process.env.REACT_APP_GEO_API_KEY as string),
+		'x-rapidapi-key': (process.env.REACT_APP_GEO_API as string),
 		'x-rapidapi-host': (process.env.REACT_APP_GEO_API_HOST as string),
 	}
 };
@@ -18,13 +18,5 @@ export const IPINFO_API_URL = (process.env.REACT_APP_IPINFO_API_BASE_URL as stri
 export const WEATHER_API_FORECAST = (process.env.REACT_APP_WEATHER_API_FORECAST_BASE_URL as string);
 export const WEATHER_API_HISTORY = (process.env.REACT_APP_WEATHER_API_HISTORY_BASE_URL as string);
 
-// Pexels API
-export const PEXELS_API_URL_DESKTOP = (process.env.REACT_APP_PEXELS_API_BASE_URL_DESKTOP as string);
-export const PEXELS_API_URL_MOBILE = (process.env.REACT_APP_PEXELS_API_BASE_URL_MOBILE as string);
-
-export const pexelsApiOptions = {
-	method: 'GET',
-	headers: {
-		Authorization: (process.env.REACT_APP_PEXELS_API_KEY as string)
-	}
-}
+// Unsplash API For Extarnal Random Images
+export const REACT_APP_UNSPLASH_API_BASE_URL = (process.env.REACT_APP_UNSPLASH_API_BASE_URL as string);
